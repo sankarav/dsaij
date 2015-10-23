95402c69b9d89102bc5807b884eb55857b1f841a
Sankara Vadivel Dhandapani <san.gct@gmail.com>
2015-10-22 22:42:18 -0400

    Chapter-3: Scoreboard in book example. add method complete with testing

--- src/main/java/edu/ub/san/dsaij/chap_3/scoreboard/GameEntry.java
+++ src/main/java/edu/ub/san/dsaij/chap_3/scoreboard/GameEntry.java
@@ -2,6 +2,36 @@ package edu.ub.san.dsaij.chap_3.scoreboard;
 
 /**
  * Created by san on 10/22/15 for dsaij.
+ * The contents of this class is as presented in book "Data Structures & Algorithms in Java (6th Edition) by Goodrich"
  */
 public class GameEntry {
+
+    private int score;
+    private String name;
+
+    public GameEntry(int score, String name) {
+        this.score = score;
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + name + " : " + score + "}";
+    }
 }
